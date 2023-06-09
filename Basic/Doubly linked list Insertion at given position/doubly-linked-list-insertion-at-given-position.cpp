89fba77524252846bdf712bd8882d7409d36487c
dipak6962 <118111987+dipak6962@users.noreply.github.com>
2023-06-09 23:31:23 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *n = new Node(x);
+    
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
+}
+
+void printList(Node *head)
+{
+  // The purpose of below two loops is
+  // to test the created DLL
+  Node *temp=head;
+  if (temp != NULL) {
+ 
+  while (temp->next!=NULL)
+    temp=temp->next;
+  while (temp->prev!=NULL)
+   temp = temp->prev;
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL; 
+  Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+     if(root==NULL) root = head;
+  }     
+  head = root;
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+//Function to insert a new node at given position in doubly linked list.
+void addNode(Node *head, int pos, int data)
+{
+    // if (pos==0){
+    //      Node *k=new Node(data);
+    //      head->prev=k;
+    //      k->next=head;
+    //      head=k;
+    //      return ;
+    // }
+    // pos-=1;
+    Node *temp=head;
+    while(pos-- and temp){
+        temp=temp->next;
+    }
+    Node *t=new Node(data);
+    t->next=temp->next;
+    t->prev=temp;
+    if(temp->next!=0)
+    temp->next->prev=t;
+    temp->next=t;
+    // return head;
+   // Your code here
+}
